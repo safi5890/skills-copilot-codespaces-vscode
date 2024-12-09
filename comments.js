ea57693577f640937137d887a2525d2cd6e4f319
Safiullah <109414497+safi5890@users.noreply.github.com>
2024-12-09 04:38:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,30 @@
-// create web server
+// Create web browser
 
-// load http module
-var http = require("http");
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-// create http server
-http.createServer(function(req, res) {
-    // content header
-    res.writeHead(200, {'content-type': 'text/html'});
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  var path = url.parse(request.url).pathname;
 
-    // write message and signal communication is complete
-    res.end("Hello, world!");
-}).listen(8124);
+  switch (path) {
+    case '/':
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(fs.readFileSync(__dirname + '/index.html'));
+      response.end();
+      break;
+    case '/comments':
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.write(JSON.stringify(comments));
+      response.end();
+      break;
+    default:
+      response.writeHead(404);
+      response.write("404 - Page not found");
+      response.end();
+  }
+});
 
-console.log('Server running on 8124');
+// Listen on port 8000, IP defaults to
