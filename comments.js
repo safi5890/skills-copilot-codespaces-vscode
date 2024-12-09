06a15495d049563278714229e1b8515d0f032d35
Safiullah <109414497+safi5890@users.noreply.github.com>
2024-12-09 04:33:16 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,48 +1,15 @@
 // create web server
 
-// import express module
-var express = require('express');
+// load http module
+var http = require("http");
 
-// create express object
-var app = express();
-// import body-parser module
-var bodyParser = require('body-parser');
-// import mongoose module
-var mongoose = require('mongoose');
-// connect to the database
-mongoose.connect('mongodb://localhost/comments');
-// import Comment model
-var Comment = require('./models/comment');
-// set view engine
-app.set('view engine', 'ejs');
-// use body-parser
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// create http server
+http.createServer(function(req, res) {
+    // content header
+    res.writeHead(200, {'content-type': 'text/html'});
 
-// set static files
-app.use(express.static('public'));
+    // write message and signal communication is complete
+    res.end("Hello, world!");
+}).listen(8124);
 
-// route to get all comments
-app.get('/comments', function(req, res) {
-    // get all comments from the database
-    Comment.find(function(err, comments) {
-        if (err) {
-            res.send(err);
-        }
-        res.json(comments);
-    });
-});
-
-// route to create a comment
-app.post('/comments', function(req, res) {
-    // create a new comment
-    Comment.create({
-            text: req.body.text
-        }, function(err, comment) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(comment);
-        });
-}
-);
+console.log('Server running on 8124');
